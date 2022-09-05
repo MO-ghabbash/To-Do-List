efc67f15d2c69be5e349fad4924876eb94b5cd7c
Muhannad <seefsy@gmail.com>
2022-09-05 16:53:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ListItem from './components/listItem/ListItem'
+import AddItem from './components/addItem/AddItem'
+import './App.css'
+class App extends Component {
+  state = {
+    items : [
+      {id: 1, action: "first Task"},
+      {id: 2, action: "second Task"},
+      {id: 3, action: "third Task"},
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  itemDelete = (id) => {
+    let items = this.state.items
+    let i = items.findIndex( (item) => item.id === id)
+    items.splice(i, 1)
+    this.setState({
+      items: items
+    })
+  }
+  addItem = (item) => {
+    let items = this.state.items
+    items.push(item)
+    this.setState({
+      items: items
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className='title'>To Do List</h1>
+        <AddItem addItem={this.addItem}/>
+        <ListItem task={this.state.items} itemDelete={this.itemDelete}/>
+      </div>
+    )
+  }
 }
 
 export default App;
